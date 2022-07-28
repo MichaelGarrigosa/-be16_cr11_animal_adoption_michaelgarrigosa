1a7e77078d59b8dde3187eb7bba598823b1c951c
MichaelGarrigosa <mgarrigosa90@gmail.com>
2022-07-28 10:06:33 +0200

    1006

--- update.php
+++ update.php
@@ -1,8 +1,8 @@
 <?php
 require_once "db_connect.php";
 
-$animal = $_GET["animal"];
-$sql = "SELECT * FROM animal WHERE animal= $animal_id";
+$animal = $_GET["animal_id"];
+$sql = "SELECT * FROM animal WHERE animal_id = $animal_id";
 $result = mysqli_query($conn, $sql);
 $row = mysqli_fetch_assoc($result);
 
